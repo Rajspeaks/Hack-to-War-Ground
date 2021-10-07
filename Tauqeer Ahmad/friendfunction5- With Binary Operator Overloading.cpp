032c8cd1fc3874f1821da15b3f2dc9d6186c650c
adeeb <hellotauqeer@gmail.com>
2021-10-08 02:16:05 +0530

    Concepts on Friend Functions C++

---
+++ Tauqeer Ahmad/friendfunction5- With Binary Operator Overloading.cpp
@@ -0,0 +1,40 @@
+//overloading binary operator + using friend function
+#include<iostream>
+using namespace std;
+
+class complex 
+{
+	 private : 
+	    int  real, img ;
+	
+	public : 
+	   void showdata()
+	   {
+	   	cout<<real<<" + "<<img<<"i"<<endl;
+	   }
+	   void getdata()
+	   {
+	   	cout<<" Enter real :- "<<endl; 
+	   	cin>>real; 
+	   	cout<<" Enter the img "<<endl ;
+	   	cin>>img;
+	   }
+	   friend complex operator + (complex,complex);
+};
+ complex operator + (const complex c5, const complex c6 )        // here -> '&' will not be used in case of friend function
+	   {
+	   	complex temp;
+	   	temp.real = c5.real + c6.real ; 
+	   	temp.img = c5.img  + c6.img; 
+	   	return temp; 
+	   }
+int main()
+{
+	complex  c1, c2; 
+	c1.getdata();
+	c2.getdata();
+	complex c3; 
+	c3 = c1 + c2;  //c3 = operator+(c1,c2); 
+	c3.showdata(); 
+	 
+}
---
+++ Tauqeer Ahmad/friendfunction6 - With Unary operator .cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class cars 
+{
+	 private : 
+	   int cost; 
+	
+	public :
+ 		 
+ 		 void getdata()
+ 		 { 
+ 		   cout<<"Enter the cost :- "<<endl;
+ 		   cin>>cost; 
+		  }
+		void showdata()
+		{
+			 cout<<" COST after 1Rs deduction  : "<<cost<<endl;
+		}
+		friend cars operator - (cars ); 
+};
+//cars operator - ( cars  &obj)           //here again & will not be used in case of friend function
+//{                                      // here 'const' will not be used because it will set the program in read only 
+//	 cars temp;
+//	 temp.cost = --obj.cost;                 // <!-- ERROR -->
+//	 return temp; 
+//}
+
+cars operator - ( cars  obj)            // SOLUTION 
+{                                       
+	 cars temp;
+	 temp.cost = --obj.cost; 
+	 return temp; 
+}
+int main()
+{
+	 cars c1,c2; 
+	  c1.getdata();
+	  c2= -c1;
+	  c2.showdata(); 
+}
